14918e49ffcbbc9cb257c70e6f256cad8558ff69
SakibHasanCse <sakibhasan9641@gmail.com>
2022-04-28 07:27:31 +0600

    Database connection updated

--- src/core/dbConnection.js
+++ src/core/dbConnection.js
@@ -8,15 +8,30 @@ const MONGO_URI = process.env.MONGO_DB_URL || 'mongodb://localhost:27017/fileSto
 
 const dbConnection = async () => {
     try{
-        await mongoose.connect(MONGO_URI, MONGO_CONFIG);
-        const db = mongoose.connection;
-        db.on("error", console.error.bind(console, "connection error: "));
-        db.once("open", function () {
-            console.log(
-                `Db connected, db name: ${db[0].name}`
-            );
+         mongoose.connect(MONGO_URI, MONGO_CONFIG);
+
+            // CONNECTION EVENTS
+            // When successfully connected
+        mongoose.connection.on('connected', function () {
+            console.log('Mongoose default connection open to ' + MONGO_URI);
+        });
+        // If the connection throws an error
+        mongoose.connection.on('error',function (err) {
+            console.log('Mongoose default connection error: ' + err);
         });
 
+        // When the connection is disconnected
+        mongoose.connection.on('disconnected', function () {
+            console.log('Mongoose default connection disconnected');
+        });
+
+        // If the Node process ends, close the Mongoose connection
+        process.on('SIGINT', function() {
+            mongoose.connection.close(function () {
+                console.log('Mongoose default connection disconnected through app termination');
+                process.exit(0);
+            });
+        });
     }catch (error) {
         console.log(
             `Failed db connection - ${error}`
